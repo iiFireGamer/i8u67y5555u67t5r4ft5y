9eb8a238275b1f97bc65be9fd5966a834fd421cb
iiFireGamer <42865927+iiFireGamer@users.noreply.github.com>
2018-09-14 22:32:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const moment = require("moment")
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -216,5 +217,19 @@ client.on('message', message => {
   client.user.setGame(`$bc `,"http://twitch.tv/S-F")
   client.user.setStatus("dnd")
   });
+
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+        m.ban();
+    };
+});
+function parseDate(str) {
+    var mdy = str.split('/');
+    return new Date(mdy[2], mdy[0]-1, mdy[1]);
+};
+
+function datediff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+};
 
 client.login ("NDg1MDg2Mzk3NDUyMjU1MjQy.Dn1h2g.bG-q-8Zm5CB0jG3_3bf0utMKUjc");
